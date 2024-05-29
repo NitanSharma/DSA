41bba7a388275fa9fc4a953b2f55331f33d01c2d
NitanSharma <sharmanitan938@gmail.com>
2024-05-29 10:41:20 +0530

    Print SubArrays

--- Array/SubArrays.java
+++ Array/SubArrays.java
@@ -1,12 +1,12 @@
 public class SubArrays {
     public static void printSubarrays(int numbers[]){
-        int ts =0;       
+        int ts =0;      
         for(int i=0; i<numbers.length; i++){// start index of subarray            
             int start = i;
             for(int j=i; j<numbers.length; j++){// endindex of subarray
                 int end = j;
                 for(int k=start; k<=end; k++){// print of sub array according to start and end
-                    System.out.print(numbers[k]+" ");                   
+                    System.out.print(numbers[k]+" ");             
                 }
                 ts++;
                 System.out.println();
